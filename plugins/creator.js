844e518c942f4897f1f1a13bb960e03798319861
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-06-24 16:12:43 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = async(m, { conn }) => {
-  let contact = await conn.sendContact(m.chat, [[owner[0] + '@s.whatsapp.net', 'Fadli'], [owner[1] + '@s.whatsapp.net', 'Zaki'], [owner[2] + '@s.whatsapp.net', 'Rafli']], m)
+  let contact = await conn.sendContact(m.chat, [[owner[0] + '@s.whatsapp.net', 'PRATAMA'], [owner[1] + '@s.whatsapp.net', 'WINDY'], [owner[2] + '@s.whatsapp.net', 'ANGGRA']], m)
   await conn.reply(m.chat, 'Nomor *owner* itu bukan nomor bot.\nHarap pengertian nya nggak ngechat command bot ke nomer owner. Makasih.', contact)
 }
 handler.help = ['owner', 'creator']
