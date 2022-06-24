fad099ad0de23e5b57a225b4bd65d4de1dfddee2
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-06-24 16:14:14 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,10 +2,10 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn }) => {
     let teks = `
 ┌──「 Donasi 」
-├ Dana: 628112958665
-├ OVO: 628112958665
-├ GoPay: 628112958665
-├ Pulsa: 6281943265086
+├ Dana:-
+├ OVO:-
+├ GoPay: +62 812-7367-7810
+├ Pulsa: +62 819-5929-3465
 └───
 `.trim()
     const message = {
